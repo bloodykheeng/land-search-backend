58f86a24d67fcc71136418798c7042c4e8a24558
bloodykheeng <bloodykheeng@gmail.com>
2022-11-26 05:41:58 +0300

    2comit

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const app = express();
-const port = process.env.Port || 3001;
-const cors = require("cors")
+const port = process.env.PORT || 3001;
+const cors = require("cors");
 const bodyparser = require("body-parser");
 const cookieparser = require("cookie-parser");
 const fileupload = require("express-fileupload");
 
 const Router = require("./router");
 
-app.use(cors({
-    credentials:true,
-    origin:"http://localhost:3000",
-    methods:["GET","POST"]
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}))
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(cookieparser());
 app.use(fileupload());
-app.use("/",Router);
+app.use("/", Router);
 
-
-app.listen(port,()=>{
-    console.log(`server started on port : ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server started on port : ${port}`);
+});
