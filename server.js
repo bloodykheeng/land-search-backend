bc6eeca3f8ada5925da0f8b2208b0119cfeb09c0
bloodykheeng <bloodykheeng@gmail.com>
2022-11-29 11:39:26 +0300

    serving client routes in build

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 //app.use(express.json());
 app.use(cookieparser());
 app.use(fileupload());
-app.use(express.static(__dirname, "build"));
+app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
